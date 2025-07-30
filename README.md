bb8115f7561e95f5874f9dabcdb4f92aeb5f4e9c
NeoSahadeo <neosahadeo@protonmail.com>
2025-07-30 22:51:56 +0200

    :tada: First kermit

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 
 The backend account handler for [NeoFlixWeb](https://github.com/NeoSahadeo/NeoFlixWeb)
 and `NeoFlixMobile`
+
+```bash
+fastapi dev src/main.py
+```
+
+# Getting Started
+
+---
+
+# Authors
+
+@ NeoSahadeo
--- src/main.py
+++ src/main.py
@@ -1,8 +1,24 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+@app.post("/login")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    ...
+
+
+@app.post("/logout")
+async def logout(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    ...
+
+
+@app.get("/logoutall")
+async def logoutall(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    ...
